742cc32e874e43cd2593935c29d4e1724d51d439
Ace G <aceguevarra48@gmail.com>
2024-08-13 17:34:32 +0800

    fix: Navigation overflow

--- src/components/DynamicNavigation.tsx
+++ src/components/DynamicNavigation.tsx
@@ -19,7 +19,7 @@ import React from "react";
 import { cn } from "@/lib/utils";
 
 function DynamicNavigation() {
-  const matches = useMediaQuery("(min-width: 768px)");
+  const matches = useMediaQuery("(min-width: 1024px)");
   return matches ? <DesktopNavigation /> : <MobileNavigation />;
 }
 
@@ -31,7 +31,7 @@ function DesktopNavigation() {
           <NavigationMenuItem>
             <NavigationMenuTrigger>About Us</NavigationMenuTrigger>
             <NavigationMenuContent>
-              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
                 {components.map((component) => (
                   <ListItem
                     key={component.title}
